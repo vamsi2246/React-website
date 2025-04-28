728de94e7852c6bda34fe854fa383497bfd4a91f
vamsi2246 <kummaravamsi2246@gmail.com>
2025-04-28 21:36:31 +0530

    Add CurrencyConverter component with basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import CurrencyConverter from './Components/CurrencyConverter'
 
 function App() {
   
 
   return (
     <>
-    <Navbar />
+    <CurrencyConverter />
     </>
   )
 }
